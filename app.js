95173da2492a081fb1137ccf08dc58a733bd4629
Tooprs <65326656+Tooprs@users.noreply.github.com>
2020-11-08 16:32:47 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const form = document.querySelector('#register');
+let userId = '';
+async function main() {
+ await liff.init({ liffId: "1654209143-W6aLAa0A" });
+ liff.ready.then(() => {
+   if (!liff.isLoggedIn()) {
+     liff.login()
+   };
+   liff.getProfile().then(profile =>{
+     userId = profile.userId;
+   }) 
+ })
+}
+main();
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -11,17 +24,3 @@ form.addEventListener('submit', (e) => {
     liff.closeWindow();
 });
 
-let userId = '';
-async function main() {
- await liff.init({ liffId: "1654209143-W6aLAa0A" });
- liff.ready.then(() => {
-   if (!liff.isLoggedIn()) {
-     liff.login()
-   };
-   liff.getProfile().then(profile =>{
-     userId = profile.userId;
-   }) 
- })
-
-}
-main();
