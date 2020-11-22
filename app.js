a8c3fbbbf23c908e3fea774c24443c15d2cf9362
Tooprs <65326656+Tooprs@users.noreply.github.com>
2020-11-22 13:21:27 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ form.addEventListener('submit', (e) => {
     fetch(`https://api.line.me/v2/bot/user/${userId}/richmenu/richmenu-b053d3370a5f863a61a5a06ca2076b6f`,{
       method: 'POST',
       headers: {
-        authorization: 'Bearer VEf+ipeUMMp/QonudMHpdyHPWfCIk24QiHpKMzdXpqPv2J3/US7PzbUY0GypLLVc6/4RbBse0rNdQjk86H9axkx02YojOgDiTDoGx7dtYIHMwijYljImXXmZZ8KryIkiXVYZW+B+XKLR6PDNXSqGCAdB04t89/1O/w1cDnyilFU='
+        authorization: 'Bearer VEf+ipeUMMp/QonudMHpdyHPWfCIk24QiHpKMzdXpqPv2J3/US7PzbUY0GypLLVc6/4RbBse0rNdQjk86H9axkx02YojOgDiTDoGx7dtYIHMwijYljImXXmZZ8KryIkiXVYZW+B+XKLR6PDNXSqGCAdB04t89/1O/w1cDnyilFU='} 
     });
     liff.closeWindow();
 });
