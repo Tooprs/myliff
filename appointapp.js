aff3525311c59ab29db598cfaa3478395c028d95
Tooprs <65326656+Tooprs@users.noreply.github.com>
2020-12-06 22:35:04 +0700

    Add files via upload

--- appointapp.js
+++ appointapp.js
@@ -1,14 +1,14 @@
 const form = document.querySelector('#appointment');
 let appointDate = '';
 async function main() {
- await liff.init({ liffId: "1654209143-W6aLAa0A" });
+ await liff.init({ liffId: "1655321436-5OMNvO2Z" });
  liff.ready.then(() => {
    if (!liff.isLoggedIn()) {
      liff.login()
    }; 
  })
 }
-
+main();
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     appointDate = form.date.value;
