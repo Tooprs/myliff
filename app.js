abae0f3e73d48a89d2d122612f021e253a5b1e80
Tooprs <65326656+Tooprs@users.noreply.github.com>
2020-12-27 13:45:23 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('#register');
+
 let userId = '';
 async function main() {
  await liff.init({ liffId: "1655373907-XvokqV8O" });
@@ -14,14 +15,14 @@ async function main() {
 main();
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(userId);
+   // e.preventDefault();
+  //console.log(userId);
     db.collection('users').doc(userId).set({
-        hn : form.hn.value,
-        name : form.name.value,
-        surname : form.surname.value,
-        personId : form.id.value,
-        telNum : form.tel.value
+        hn : document.getElementById("hn").value,
+        name : document.getElementById("name").value,
+        surname : document.getElementById("surname").value,
+        lineId : document.getElementById("lineid").value,
+        telNum : document.getElementById("tel").value
     });
     
     liff.closeWindow();
