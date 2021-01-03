90ebc59bceb1dae1f70a175bc0b95479f43ee9c8
Tooprs <65326656+Tooprs@users.noreply.github.com>
2021-01-03 19:16:51 +0700

    Add files via upload

--- appointapp.js
+++ appointapp.js
@@ -1,3 +1,16 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBT2kp4K49JZZld2x0DlPc26htXAy6ZAkU",
+  authDomain: "line-bot-7769c.firebaseapp.com",
+  databaseURL: "https://line-bot-7769c.firebaseio.com",
+  projectId: "line-bot-7769c",
+  storageBucket: "line-bot-7769c.appspot.com",
+  messagingSenderId: "318833172832",
+  appId: "1:318833172832:web:c7761021702b5f14bc7e05",
+  measurementId: "G-SMTZR47DPY"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 const form = document.querySelector('#appointment');
 let userId = '';
 let appointDate = '';
@@ -17,11 +30,31 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     appointDate = form.date.value;
     doctorName = form.doctor.value;
+    let Name;
+    let Surname;
+    let HN;
+    db.collection('users').doc(userId).get().then((doc)=>{
+      Name = doc.data().name;
+      Surname = doc.data().surname;
+      HN = doc.data().hn;
+      });
     db.collection('users').doc(userId).update({
        appointment : appointDate,
        doctor : doctorName 
     }).then(() =>{
-      liff.closeWindow();
+      liff.shareTargerPicker([
+        {
+          type: 'text',
+          text: `ยื่นนัดออนไลน์
+          ${Name}  ${Surname}
+          ${HN}
+          นัดพบแพทย์ ${doctorName}
+          ${appointDate}`
+        }
+      ]).then((res) => {
+        liff.closeWindow();
+      })
+      
     });
     
     
