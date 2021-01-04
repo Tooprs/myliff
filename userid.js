baf9d6b7ece58075010f4857ca9f7da0db469cbc
Tooprs <65326656+Tooprs@users.noreply.github.com>
2021-01-04 23:30:22 +0700

    Add files via upload

---
+++ userid.js
@@ -0,0 +1,15 @@
+let userId = '';
+async function main() {
+ await liff.init({ liffId: "1655541441-GL6enzey" });
+ liff.ready.then(() => {
+   if (!liff.isLoggedIn()) {
+     liff.login()
+   };
+   liff.getProfile().then(profile =>{
+     userId = profile.userId;
+   }) 
+ })
+}
+main();
+
+console.log(userId)
