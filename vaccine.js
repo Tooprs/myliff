75ad49d787afe627ae8dc40d01c88484802421ef
Tooprs <65326656+Tooprs@users.noreply.github.com>
2021-05-26 23:43:18 +0700

    Add files via upload

--- vaccine.js
+++ vaccine.js
@@ -1,6 +1,5 @@
 
-
-let url = 'https://script.google.com/macros/s/AKfycbyiI9nX3mCx5pQlSTNS55W2WxVroVJubgN4oFkXspBtCppU6z1p5b1glWKZucSdg8TiRQ/exec';
+let url = 'https://script.google.com/macros/s/AKfycbyv1KGVBO30ncssxgxo5EJZfHpLezSs94reEb4Q0PJjlhpJJZyx7CfyRQOdR-IEVQYuug/exec';
 document.getElementById("myBtn").addEventListener("click", (e)=>{
     e.preventDefault();
     const idUpload = document.getElementById("id").value;
@@ -8,31 +7,33 @@ document.getElementById("myBtn").addEventListener("click", (e)=>{
     document.querySelector('ul').innerHTML = '<li>กรุณารอสักครู่....</li>';
     fetch(url).then(response => response.json())
     .then(data => {
+        
         let resultName;
         let resultDate;
         let resultTime;
         let resultQueue;
-        let notAllow = false;
+        let resultPlace;
+        //let notAllow = false;
         for(var i=0; i<data[0].data.length;i++){
-            if(data[0].data[i][2] == idUpload) {
+            if(data[0].data[i][1] == idUpload) {
                 resultName = data[0].data[i][0];
-                resultDate = data[0].data[i][9];
-                resultTime = data[0].data[i][10];
-                resultQueue = data[0].data[i][13];
-                if(resultDate ==''){
-                    notAllow = true;
-                }
+                resultDate = data[0].data[i][4];
+                resultTime = data[0].data[i][5];
+                resultPlace = data[0].data[i][6];
+                resultQueue = data[0].data[i][7];
+                // if(resultDate ==''){
+                //     notAllow = true;
+                // }
             }
         }
         if(resultName==undefined){
-            html = '<li>ไม่พบการลงทะเบียน</li> <li>(หากท่านลงทะเบียนแล้ว กรุณาติดต่อเจ้าหน้าที่ ณ จุดประชาสัมพันธ์)</li>'
-        }else if(notAllow==true){
-            html = '<li>ขออภัย ท่านไม่เข้าเกณฑ์ฉีดวัคซีนของโรงพยาบาลในรอบนี้ กรุณารอรอบถัดไป</li>'
-        }else{
+            html = '<li>ไม่พบการลงทะเบียน</li> <li>กรุณาตรวจสอบความถูกต้องของเลขที่กรอกอีกครั้ง</li><li>(หากท่านลงทะเบียนแล้ว กรุณาติดต่อเจ้าหน้าที่ ณ จุดประชาสัมพันธ์)</li>'
+        }
+        else{
             html = `<li> ${resultName}</li>
             <li>มีนัดฉีดวัคซีนวันที่ ${resultDate}</li>
             <li>เวลา ${resultTime}</li>
-            <li>เดอะมอลล์ บางกะปิ</li>
+            <li>สถานที่ ${resultPlace}</li>
             <li>หมายเลขคิว ${resultQueue}</li>
             <li>ตรวจสอบลำดับชื่อและยื่นลำดับให้เจ้าหน้าที่ลงทะเบียน</li>`
         }
@@ -41,13 +42,15 @@ document.getElementById("myBtn").addEventListener("click", (e)=>{
     });
 });
 
-
-//id = data[0].data[i][2]
+//id = data[0].data[i][1]
 //name = data[0].data[i][0]
-//date = data[0].data[i][9]
-//time = data[0].data[i][10]
-//queue = data[0].data[i][13]
+//date = data[0].data[i][4]
+//time = data[0].data[i][5]
+//queue = data[0].data[i][7]
+//place = data[0].data[i][6]
 //test: https://script.google.com/macros/s/AKfycbyAjSc-gf65WxOW6ozq9K7bmryyPUrofgE7L3Pw0NGsEjuIfV4oxJMUu_Jma9M453s-/exec
 //true: https://script.google.com/macros/s/AKfycbwzsaMH1xRPhihUx4h4iZXgUsJqXg5udfOU1t6j5oKeIJv7rrAONgsuaWG-gjOJn82Y/exec
-//test2: https://script.google.com/macros/s/AKfycbyiI9nX3mCx5pQlSTNS55W2WxVroVJubgN4oFkXspBtCppU6z1p5b1glWKZucSdg8TiRQ/exec
+//previously use: https://script.google.com/macros/s/AKfycbyiI9nX3mCx5pQlSTNS55W2WxVroVJubgN4oFkXspBtCppU6z1p5b1glWKZucSdg8TiRQ/exec
 //https://script.google.com/macros/s/AKfycbxIBTqU_8Q-A_ou_Fzjm-BF-5M7cT6mlCcVrjePlpKR39unXX9hatjXxCRl_-JV6Syd/exec
+
+//new : https://script.google.com/macros/s/AKfycbyv1KGVBO30ncssxgxo5EJZfHpLezSs94reEb4Q0PJjlhpJJZyx7CfyRQOdR-IEVQYuug/exec
